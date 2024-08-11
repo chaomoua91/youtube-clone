b749f288b3dc9427fb14feab6737e926f88d5367
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-08-11 18:42:12 -0500

    chore: Update Button component and PageHeader layout

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import{cva} from "class-variance-authority"
+import React from "react";
+import { cva } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+export const buttonStyles = cva(
+  ["hover:bg-secondary-hover", "transition-colors"],
+  {
+    variants: {
+      default: ["bg-secondary"],
+      ghost: ["hover:bg-gray-100"],
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  }
+);
 
-export const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors" ]){
-}
-
-
-function Button() {
+function Button({ variants, size, className, ...props }) {
   return (
-
-  )
+    <button
+      {...props}
+      className={twMerge(buttonStyles({ variants, size }), className)}
+    />
+  );
 }
 
-export default Button
+export default Button;
--- src/layouts/PageHeader.jsx
+++ src/layouts/PageHeader.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Menu } from "lucide-react";
 import logo from "../assets/fakeyoutubelogo.png";
+import Button from "../components/Button";
 
 function PageHeader() {
   return (
     <div className="flex gap-10 lg:gap-20 justify-between">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <button>
+        <Button>
           <Menu />
-        </button>
+        </Button>
         <a href="/">
           <img src={logo} alt="fake youtube logo" />
         </a>
