a8b6f49f00b4b1d6eb1ee93fb8e1dfa7b9bf5828
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-08-12 18:28:36 -0500

    feat: Add lazy loading for video thumbnails in VideoGridItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PageHeader from "./layouts/PageHeader";
 import CategoryPills from "./components/CategoryPills";
-import { categories } from "./data/home";
+import { categories, videos } from "./data/home";
 import { useState } from "react";
 import VideoGridItem from "./components/VideoGridItem";
 
@@ -25,7 +25,9 @@ function App() {
             />
           </div>
           <div className="grid gap-4 grid-cols-[repeat(auto-fill, minmax(300px,1fr))]">
-            <VideoGridItem />
+            {videos.map((video) => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/VideoGridItem.jsx
+++ src/components/VideoGridItem.jsx
@@ -33,6 +33,12 @@ function VideoGridItem({
           src={thumbnailUrl}
           className="block w-full object-cover rounded-xl"
         />
+        <div
+          className="absolute bottom-1 right-1 bg-secondary-dark text-secondary
+        text-sm px-0.5 rounded"
+        >
+          {duration}
+        </div>
       </a>
     </div>
   );
