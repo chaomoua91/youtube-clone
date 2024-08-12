e2bb840af4dcd948cf8846badf999ac33868ef07
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-08-12 18:57:38 -0500

    feat: Add formatDuration and formatTimeAgo utility functions for VideoGridItem component

--- src/components/VideoGridItem.jsx
+++ src/components/VideoGridItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { formatDuration } from "../utils/formatDuration";
+import { formatTimeAgo } from "../utils/formatTimeAgo";
 
 VideoGridItem.propTypes = {
   id: PropTypes.string.isRequired,
@@ -15,6 +17,10 @@ VideoGridItem.propTypes = {
   thumbnailUrl: PropTypes.string.isRequired,
   videoUrl: PropTypes.string.isRequired,
 };
+
+const VIEW_FORMATTER = new Intl.NumberFormat(undefined, {
+  notation: "compact",
+});
 
 function VideoGridItem({
   id,
@@ -37,9 +43,25 @@ function VideoGridItem({
           className="absolute bottom-1 right-1 bg-secondary-dark text-secondary
         text-sm px-0.5 rounded"
         >
-          {duration}
+          {formatDuration(duration)}
         </div>
       </a>
+      <div className="flex gap-2">
+        <a href={`/@${channel.id}`} className="flex-shrink-0">
+          <img className="w-12 h-12 rounded-full" src={channel.profileUrl} />
+        </a>
+        <div className="flex flex-col ">
+          <a href={`/watch?v=${id}`} className="font-bold">
+            {title}
+          </a>
+          <a href={`/@${channel.id}`} className="text-secondary-text text-sm">
+            {channel.name}
+          </a>
+          <div className="text-secondary-text text-sm">
+            {VIEW_FORMATTER.format(views)} views • {formatTimeAgo(postedAt)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
