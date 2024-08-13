c50bc67b90013e07c2d5fbd06b512b1da0953d92
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-08-12 19:37:17 -0500

    feat: Add Sidebar component for sidebar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CategoryPills from "./components/CategoryPills";
 import { categories, videos } from "./data/home";
 import { useState } from "react";
 import VideoGridItem from "./components/VideoGridItem";
+import Sidebar from "./layouts/SideBar";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
@@ -11,12 +12,11 @@ function App() {
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
-
       <div
         className="grid grid-cols-[auto,1fr] 
            flex-grow-1 overflow-auto"
       >
-        <div>SideBar</div>
+        <Sidebar />
         <div className="overflow-x-hidden px-8 pb-4">
           <div className="sticky top-0 bg-white z-10 pb-4">
             <CategoryPills
---
+++ src/contexts/SidebarContext.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SidebarContext() {
+  return null;
+}
+
+export default SidebarContext;
