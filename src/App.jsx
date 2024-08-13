e92de3b8df94b29eeb5e77535fd1a330326e4cff
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-08-12 19:39:36 -0500

    refactor: Simplify grid column definition in App component

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,7 @@ function App() {
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
-      <div
-        className="grid grid-cols-[auto,1fr] 
-           flex-grow-1 overflow-auto"
-      >
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
         <Sidebar />
         <div className="overflow-x-hidden px-8 pb-4">
           <div className="sticky top-0 bg-white z-10 pb-4">
@@ -25,7 +22,7 @@ function App() {
               onSelect={setSelectedCategory}
             />
           </div>
-          <div className="grid gap-4 grid-cols-[repeat(auto-fill, minmax(300px,1fr))]">
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
             {videos.map((video) => (
               <VideoGridItem key={video.id} {...video} />
             ))}
